2db24d00df40bec28634d1664639d213f54b4b8d
sooya12 <hyeonsu2014@naver.com>
2021-01-16 00:33:40 +0900

    initial | LeaseComplexListDto 생성

---
+++ src/main/java/project/personal/lhinfo/dto/LeaseComplexListDto.java
@@ -0,0 +1,12 @@
+package project.personal.lhinfo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LeaseComplexListDto {
+    public String location;
+    public String supplyType;
+    public String page;
+}
