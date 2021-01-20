67a8c3ef0c72ec3bd3daf762d2ac1a69c2eba484
sooya12 <hyeonsu2014@naver.com>
2021-01-20 12:53:29 +0900

    update | open 화면과 home 화면 분리

--- src/main/java/project/personal/lhinfo/controller/HomeController.java
+++ src/main/java/project/personal/lhinfo/controller/HomeController.java
@@ -3,6 +3,7 @@ package project.personal.lhinfo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,9 +13,16 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String open() {
+		logger.info("분양임대 정보 제공 서비스 - open");
+
+		return "open";
+	}
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
-		logger.info("분양임대 정보 제공 서비스!");
-		
+		logger.info("분양임대 정보 제공 서비스 - home");
+
 		return "home";
 	}
 }
