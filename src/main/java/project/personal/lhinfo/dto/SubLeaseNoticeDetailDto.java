e1baea0fc43260d3a46119b8413b56cb867213fb
sooya12 <hyeonsu2014@naver.com>
2021-01-26 17:05:43 +0900

    update | 주석 추가

--- src/main/java/project/personal/lhinfo/dto/SubLeaseNoticeDetailDto.java
+++ src/main/java/project/personal/lhinfo/dto/SubLeaseNoticeDetailDto.java
@@ -13,27 +13,27 @@ import java.util.List;
 public class SubLeaseNoticeDetailDto {
     public List<resHeader> resHeader;
     public List<dsCtrtPlcNm> dsCtrtPlcNm;
-    public List<dsCtrtPlc> dsCtrtPlc;
-    public List<dsSbdNm> dsSbdNm;
-    public List<dsSbd> dsSbd;
-    public List<dsSbdAhflNm> dsSbdAhflNm;
-    public List<dsSbdAhfl> dsSbdAhfl;
-    public List<dsSplScdlNm> dsSplScdlNm;
-    public List<dsSplScdl> dsSplScdl;
-    public List<dsSplScdl01Nm> dsSplScdl01Nm;
-    public List<dsSplScdl01> dsSplScdl01;
-    public List<dsSplScdl02> dsSplScdl02;
-    public List<dsSplScdl02Nm> dsSplScdl02Nm;
-    public List<dsAhflInfoNm> dsAhflInfoNm;
-    public List<dsAhflInfo> dsAhflInfo;
-    public List<dsEtcInfoNm> dsEtcInfoNm;
-    public List<dsEtcInfo> dsEtcInfo;
-    public List<dsEtcListNm> dsEtcListNm;
-    public List<dsEtcList> dsEtcList;
-    public List<dsSbdDongAhflNm> dsSbdDongAhflNm;
-    public List<dsSbdDongAhfl> dsSbdDongAhfl;
+    public List<dsCtrtPlc> dsCtrtPlc;               // 접수처 정보
+    public List<dsSbdNm> dsSbdNm;                   
+    public List<dsSbd> dsSbd;                       // 단지 정보
+    public List<dsSbdAhflNm> dsSbdAhflNm;            
+    public List<dsSbdAhfl> dsSbdAhfl;               // 단지 첨부파일 정보 
+    public List<dsSplScdlNm> dsSplScdlNm;           
+    public List<dsSplScdl> dsSplScdl;               // 공급 정보
+    public List<dsSplScdl01Nm> dsSplScdl01Nm;        
+    public List<dsSplScdl01> dsSplScdl01;           // 공급일정-입찰 정보
+    public List<dsSplScdl02Nm> dsSplScdl02Nm;        
+    public List<dsSplScdl02> dsSplScdl02;           // 공급일정-추첨 정보 
+    public List<dsAhflInfoNm> dsAhflInfoNm;          
+    public List<dsAhflInfo> dsAhflInfo;             // 첨부파일 정보
+    public List<dsEtcInfoNm> dsEtcInfoNm;            
+    public List<dsEtcInfo> dsEtcInfo;               // 기타 정보
+    public List<dsEtcListNm> dsEtcListNm;            
+    public List<dsEtcList> dsEtcList;               // 신청자격 정보
     public List<dsSbdDongNm> dsSbdDongNm;
-    public List<dsSbdDong> dsSbdDong;
+    public List<dsSbdDong> dsSbdDong;               // 단지내상가동 정보
+    public List<dsSbdDongAhflNm> dsSbdDongAhflNm;
+    public List<dsSbdDongAhfl> dsSbdDongAhfl;       // 단지내상가동별이미지 정보
 
     @Override
     public String toString() {
@@ -49,18 +49,18 @@ public class SubLeaseNoticeDetailDto {
                 ", dsSplScdl=" + dsSplScdl +
                 ", dsSplScdl01Nm=" + dsSplScdl01Nm +
                 ", dsSplScdl01=" + dsSplScdl01 +
-                ", dsSplScdl02=" + dsSplScdl02 +
                 ", dsSplScdl02Nm=" + dsSplScdl02Nm +
+                ", dsSplScdl02=" + dsSplScdl02 +
                 ", dsAhflInfoNm=" + dsAhflInfoNm +
                 ", dsAhflInfo=" + dsAhflInfo +
                 ", dsEtcInfoNm=" + dsEtcInfoNm +
                 ", dsEtcInfo=" + dsEtcInfo +
                 ", dsEtcListNm=" + dsEtcListNm +
                 ", dsEtcList=" + dsEtcList +
-                ", dsSbdDongAhflNm=" + dsSbdDongAhflNm +
-                ", dsSbdDongAhfl=" + dsSbdDongAhfl +
                 ", dsSbdDongNm=" + dsSbdDongNm +
                 ", dsSbdDong=" + dsSbdDong +
+                ", dsSbdDongAhflNm=" + dsSbdDongAhflNm +
+                ", dsSbdDongAhfl=" + dsSbdDongAhfl +
                 '}';
     }
 }
