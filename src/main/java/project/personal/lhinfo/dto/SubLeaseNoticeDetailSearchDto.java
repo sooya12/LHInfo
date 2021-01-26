4bfdbe28b499e23a921e57badf65322ace9fdc78
sooya12 <hyeonsu2014@naver.com>
2021-01-26 16:26:01 +0900

    initial | 분양임대 공고문 상세 조회를 위한 매개값 Dto

---
+++ src/main/java/project/personal/lhinfo/dto/SubLeaseNoticeDetailSearchDto.java
@@ -0,0 +1,27 @@
+package project.personal.lhinfo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SubLeaseNoticeDetailSearchDto {
+    public String AIS_TP_CD;            // 매물유형코드
+    public String SPL_INF_TP_CD;        // 공급정보구분코드
+    public String PAN_ID;               // 공고 ID
+    public String UPP_AIS_TP_CD;        // 상위매물유형코드
+    public String CCR_CNNT_SYS_DS_CD;   // 고객센터연계시스템구분코드
+
+    @Override
+    public String toString() {
+        return "SubLeaseNoticeDetailDto{" +
+                "AIS_TP_CD='" + AIS_TP_CD + '\'' +
+                ", SPL_INF_TP_CD='" + SPL_INF_TP_CD + '\'' +
+                ", PAN_ID='" + PAN_ID + '\'' +
+                ", UPP_AIS_TP_CD='" + UPP_AIS_TP_CD + '\'' +
+                ", CCR_CNNT_SYS_DS_CD='" + CCR_CNNT_SYS_DS_CD + '\'' +
+                '}';
+    }
+}
